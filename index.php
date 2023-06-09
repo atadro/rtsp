cfab7f76bf60ef557e7624db02e2192afc382c79
atadro <136048637+atadro@users.noreply.github.com>
2023-06-09 11:30:07 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<script src="webrtcplayer.js"></script>
+<?php include 'runcommand.php';?>
+
+
+<video id="videoElem" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem2" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem3" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem4" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem5" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem6" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem7" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem8" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem9" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem10" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem11" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem12" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem13" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem14" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem15" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem16" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem17" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem18" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem19" autoplay controls muted style="width:250px; height:160px;"></video>
+<video id="videoElem20" autoplay controls muted style="width:250px; height:160px;"></video>
+
+
+
+<script src="playerlist.js"></script>
